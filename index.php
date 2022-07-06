59a2c0104c7ab48fa4e79a6bea61d537fff815a3
lucas <gepeoverload@gmail.com>
2022-07-06 17:11:53 +0200

    added basic logic to index

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 declare(strict_types=1);
-require 'Views/home.php';
+
+//Require Models
+require 'Models/User.php';
+
+//Require Controllers
+require 'Controllers/HomeController.php';
+
+//Instantiate a new default Controller
+$controller = new HomeController();
+
+//Tell the controller to display a view by calling the render function
+$controller->render();
