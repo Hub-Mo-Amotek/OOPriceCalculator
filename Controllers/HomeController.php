33a569ebcbc2a32946cf86d576a38313526aa2c6
lucas <gepeoverload@gmail.com>
2022-07-07 12:29:15 +0200

    added a check for the env file

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -4,6 +4,11 @@ class HomeController {
 
     public function render(){
 
+        $user = new User('Mo', 'Le');
+        $data = $user->retrieveCustomers();
+
+        var_dump($data);
+
         require 'Views/home.php';
 
     }
