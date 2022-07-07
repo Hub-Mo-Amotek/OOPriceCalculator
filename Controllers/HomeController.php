e15f57d04d55e1302198d354d1969f0d12af318d
lucas <gepeoverload@gmail.com>
2022-07-07 17:03:23 +0200

    changed method name of customer class and removed test code from the infopage controller

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -4,11 +4,6 @@ class HomeController {
 
     public function render(){
 
-        $user = new User('Mo', 'Le');
-        $data = $user->retrieveCustomers();
-
-        var_dump($data);
-
         require 'Views/home.php';
 
     }
--- Models/User.php
+++ Models/User.php
@@ -2,41 +2,13 @@
 
 class User extends DataSource{
 
-private string $name;
-private string $surname;
-
-public function __construct(string $name, string $surname){
-
-    $this->name = $name;
-    $this->surname = $surname;
-
-}
-
-public function retrieveCustomers(){
+public function retrieveCustomerNames(){
 
     $responseArray = [];
 
     $dbh = $this->connect();
 
-    $sql = "SELECT * FROM Customer WHERE group_id < 10";
-
-    $query = $dbh->query($sql);
-
-    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        array_push($responseArray, $row);
-    }
-
-    return $responseArray;
-
-}
-
-public function retrieveGroupId(){
-
-    $responseArray = [];
-
-    $dbh = $this->connect();
-
-    $sql = "SELECT group_id FROM Customer WHERE firstname='Peg'";
+    $sql = "SELECT firstname, lastname FROM Customer";
 
     $query = $dbh->query($sql);
 
