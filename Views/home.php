ea93805798906832f8321d030d6988ac1dddbdea
Hub-Mo <Mohamed.elja@hotmail.com>
2022-07-08 10:58:04 +0200

    added the 2 dropdown menus to the home View with all the customers and products

--- Views/home.php
+++ Views/home.php
@@ -3,41 +3,32 @@ declare(strict_types=1);
 include 'Views/include/header.php';
 ?>
 
-<div class="dropdown">
-    <button
-        class="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton2"
-        data-mdb-toggle="dropdown"
-        aria-expanded="false"
-    >
-        Dropdown button
-    </button>
-    //<ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-    <?php
-    //$dummyArray = ['Mo El', 'Lucas S', 'Michael Monteiro', 'Hello names', 'hello planet'];
-    //    foreach($dummyArray as $name){
-    //        echo "<li value='$name' ><a class='dropdown-item' href='#'>".$name."</a></li>";
-    //    }
-    ?>
-    </ul>
+<div class="container d-flex justify-content-center">
+    <form method="post" name="page">
+        <select>
+            <option selected="selected">Choose one</option>
+            <?php
+            // A sample product array
+            // Iterating through the product array
+            foreach($customerNames as $item){
+                echo "<option value='strtolower($item->id)'>$item->name</option>";
+            }
+            ?>
+        </select>
+        <select>
+            <option selected="selected">Choose Product</option>
+            <?php
+            // A sample product array
+            // Iterating through the product array
+            foreach($productNames as $item){
+                echo "<option value='strtolower($item->id)'>$item->name</option>";
+            }
+            ?>
+        </select>
+        <button type="submit" value="Submit">Calculate</button>
+    </form>
 
 </div>
-<form method="post">
-    <select>
-        <option selected="selected">Choose one</option>
-        <?php
-        // A sample product array
-        $products = array("Michael", "Mo", "Lucas", "User");
-        // Iterating through the product array
-        foreach($products as $item){
-            echo "<option value='strtolower($item)'>$item</option>";
-        }
-        ?>
-    </select>
-    <input type="submit" value="Submit">
-</form>
-
 <?php
 include 'Views/include/footer.php';
 ?>
