0e85041943de9a8ec116d556b080eec1ac22966e
lucas <gepeoverload@gmail.com>
2022-07-11 09:12:00 +0200

    modified user class to have a private property related groups

--- Models/User.php
+++ Models/User.php
@@ -8,8 +8,9 @@ class User {
     private int $groupId;
     private $fixed_discount;
     private $variable_discount;
+    private array $related_groups;
     
-    public function __construct(array $dataRow){
+    public function __construct(array $dataRow, array $related_groups){
 
         $this->id = $dataRow['id'];
         $this->firstName = $dataRow['firstname'];
@@ -17,6 +18,7 @@ class User {
         $this->groupId = $dataRow['group_id'];
         $this->fixed_discount = $dataRow['fixed_discount'];
         $this->variable_discount = $dataRow['variable_discount'];
+        $this->related_groups = $related_groups;
 
     }
 
@@ -24,6 +26,12 @@ class User {
 
         return $this->groupId;
             
+    }
+
+    public function getRelatedGroups(){
+
+        return $this->related_groups;
+
     }
 
 
